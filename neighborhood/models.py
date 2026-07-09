e75555a51d52bb3384f2266a34c8c4e91c820035
Sergio-zt <fop.gso@gmail.com>
2026-07-09 12:33:30 -0400

    Add User, District and Post model to models.py

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class District(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class User(AbstractUser):
+    district = models.ManyToManyField(District, related_name='users', blank=True)
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    post_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    text = models.TextField()
